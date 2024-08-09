e9db9d81bcd7aa0c019d628909116dd316dc5993
Clément Terrien <clement.terrien@hotmail.fr>
2024-08-09 15:43:11 +0200

    Changing text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I've been changed agzin
+          Luis... Tu pues <3
         </p>
         <a
           className="App-link"
